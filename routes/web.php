6ebd3315ebb7d81f9a5674061a7368735462aed3
RobertoSamuelM <roberto1maciel1@gmail.com>
2024-10-11 01:58:26 -0300

    rutas

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,8 @@ use App\Http\Controllers\ProductoServicioController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Redirigir la ruta raíz al índice de productos/servicios
+Route::get('/', [ProductoServicioController::class, 'index'])->name('productos-servicios.index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -16,9 +15,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
     // Rutas para productos y servicios
-    Route::resource('productos-servicios', ProductoServicioController::class);
+    Route::get('/productos-servicios', [ProductoServicioController::class, 'index'])->name('productos-servicios.index');
+    Route::post('/producto-servicio', [ProductoServicioController::class, 'store'])->name('producto-servicio.store');
+    Route::get('/producto-servicio/{id}/edit', [ProductoServicioController::class, 'edit'])->name('producto-servicio.edit');
+    Route::put('/producto-servicio/{id}', [ProductoServicioController::class, 'update'])->name('producto-servicio.update');
+    Route::delete('/producto-servicio/{id}', [ProductoServicioController::class, 'destroy'])->name('producto-servicio.destroy');
+    Route::get('/producto-servicio/{id}', [ProductoServicioController::class, 'show'])->name('producto-servicio.show');
 });
 
 require __DIR__.'/auth.php';
+
